a257512a2627ece3a9edc12eb6142c10c311678e
Michael Mondoro <michaelmondoro@Michaels-MacBook-Air.local>
2024-01-20 20:37:43 -0500

    another test

--- tests/test.py
+++ tests/test.py
@@ -13,7 +13,10 @@ class WeatherTest(unittest.TestCase):
     def test_invalid_measure(self):
         with self.assertRaises(TypeError):
             Weather(22801, "invalid")
-    
+
+    def test_metric_measure(self):
+        weather = Weather(22801, "metric")
+        self.assertEqual(weather.res.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
